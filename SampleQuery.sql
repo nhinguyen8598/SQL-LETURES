5341e478043d1727c232336f651b5f0f76781bd2
nhinguyen8598 <111105002+nhinguyen8598@users.noreply.github.com>
2022-12-21 14:44:03 +0700

    LESSON 1

---
+++ SampleQuery.sql
@@ -0,0 +1,19 @@
+-- Tạo Database BikeStores
+-- Các bạn viết lệnh bên dưới comment này
+
+
+
+-- Sử dụng Database BikeStores
+-- Các bạn viết lệnh bên dưới comment này
+
+
+
+-- Tạo Schemas
+CREATE SCHEMA [production]
+GO
+
+CREATE SCHEMA [sales]
+GO
+
+-- Tạo các bảng như mô tả
+-- Các bạn viết lệnh bên dưới comment này
